79ffbc958f41b5481c2b3ee519707929aff63754
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-16 15:06:18 -0800

    style: encrypting password & creating jwt tokens

---
+++ serverside/Controllers/userController.js
@@ -0,0 +1,43 @@
+const userModel = require("../Models/userModel");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  const jwtKey = process.env.JWT_SECRET_KEY;
+
+  return jwt.sign({ _id }, jwtKey, { expiresIn: "3d" });
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    let user = await userModel.findOne({ email });
+
+    if (user) return res.status(400).json("User already exists");
+
+    if (!user || !email || !password)
+      return res.status(400).json("All fields are required");
+
+    if (!validator.isEmail(email)) return res.status(400).json("Invalid email");
+
+    if (!validator.isStrongPassword(password))
+      return res.status(400).json("Password not strong enough");
+
+    user = new userModel({ name, email, password });
+
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ _id: user._id, name, email, token });
+  } catch (error) {
+    console.log(error);
+    res.staus(400).json({ error });
+  }
+};
+
+module.exports = { registerUser };
