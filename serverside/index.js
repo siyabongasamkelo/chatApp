198d5f6d350cadd2629708ae7787c1a8a18489e7
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-16 15:12:02 -0800

    refactor: correcting spellings

--- serverside/index.js
+++ serverside/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const userRouter = require("./Routes/userRoutes");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("api/users", userRouter);
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URL;
