e8a3eadcf3d10441797f4de5c9540b7581db0a8a
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-17 13:00:07 -0800

    feat: created the register page with 3 input feilds and also added the auth context

--- clientside/chatapp/src/pages/Register.js
+++ clientside/chatapp/src/pages/Register.js
@@ -1,5 +1,43 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import NavBar from "../components/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 const Register = () => {
-  return <div>register</div>;
+  const { user } = useContext(AuthContext);
+  console.log(user);
+  return (
+    <>
+      <NavBar />
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "20%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={6}>
+              <h2 className=" text-white">Register</h2>
+              <h2 className=" text-white">{user.name}</h2>
+
+              <Form.Control type="text" placeholder="Name" />
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Password" />
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 };
 
 export default Register;
