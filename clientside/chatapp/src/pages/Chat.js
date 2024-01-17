f26654abdd3c09ed836ddc80f2ce26a88b0b49b1
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-17 12:58:07 -0800

    feat: created the chat page

--- clientside/chatapp/src/pages/Chat.js
+++ clientside/chatapp/src/pages/Chat.js
@@ -1,5 +1,12 @@
+import { Container } from "react-bootstrap";
+import NavBar from "../components/Navbar";
 const Chat = () => {
-  return <div>chat</div>;
+  return (
+    <Container>
+      <NavBar />
+      chat
+    </Container>
+  );
 };
 
 export default Chat;
