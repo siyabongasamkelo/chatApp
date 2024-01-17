c5eceb7931a304ffa46f15b425dec518330f3140
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-17 12:59:03 -0800

    eat: created the login page with 2 input feilds

--- clientside/chatapp/src/pages/Login.js
+++ clientside/chatapp/src/pages/Login.js
@@ -1,5 +1,36 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import NavBar from "../components/Navbar";
+
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <>
+      <NavBar />
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "20%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={6}>
+              <h2 className=" text-white">Login</h2>
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="Password" placeholder="Password" />
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 };
 
 export default Login;
