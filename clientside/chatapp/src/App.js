cb6d33d88780eb48d2a57dcd2a9ffcc5b2dec63e
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-18 11:08:18 -0800

    protected all my routes from unlogged users

--- clientside/chatapp/src/App.js
+++ clientside/chatapp/src/App.js
@@ -9,15 +9,18 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route index element={<Chat />} />
-        <Route path="/" element={<Chat />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route index element={user ? <Chat /> : <Login />} />
+        <Route path="/" element={user ? <Chat /> : <Login />} />
+        <Route path="/login" element={user ? <Chat /> : <Login />} />
+        <Route path="/register" element={user ? <Chat /> : <Register />} />
         <Route path="/*" element={<Chat />} />
       </Route>
     )
