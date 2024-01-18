5b80a304a090c333a9e57066156089bc01e96b7b
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-18 13:29:26 -0800

    feat: added chats & message routes

--- serverside/index.js
+++ serverside/index.js
@@ -3,12 +3,16 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRouter = require("./Routes/userRoutes");
+const chatRouter = require("./Routes/ChatRoutes");
+const messageRouter = require("./Routes/messageRoutes");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRouter);
+app.use("/api/chats", chatRouter);
+app.use("/api/messages", messageRouter);
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URL;
