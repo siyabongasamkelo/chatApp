790209d9e6af2524ff32fab6a74974812661a748
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-18 08:24:56 -0800

    feat: creating the register function on the auth context

--- clientside/chatapp/src/context/AuthContext.js
+++ clientside/chatapp/src/context/AuthContext.js
@@ -1,15 +1,52 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
+import { baseUrl, postRequest } from "../utils/Services";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: "Siya",
+  const [user, setUser] = useState(null);
+  const [registerInfo, setRegisterInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
   });
+  const [registerError, setRegisterError] = useState(null);
+  const [isRegisterLoading, setIsRegisterLoading] = useState(false);
+
+  const updateRegisterInfo = useCallback((info) => {
+    setRegisterInfo(info);
+  }, []);
+
+  console.log("registerInfo", registerInfo);
+
+  const registerUser = useCallback(async (e) => {
+    e.preventDefault();
+    alert("working");
+    setIsRegisterLoading(true);
+    setRegisterError(null);
+
+    const response = await postRequest(
+      `${baseUrl}/users/register`,
+      JSON.stringify(registerInfo)
+    );
+
+    setIsRegisterLoading(false);
+    if (response.error) {
+      return setRegisterError(response);
+    }
+
+    localStorage.setItem("User", JSON.stringify(response));
+    setUser(response);
+  }, []);
 
   return (
     <AuthContext.Provider
       value={{
         user,
+        registerInfo,
+        updateRegisterInfo,
+        registerError,
+        registerUser,
+        isRegisterLoading,
       }}
     >
       {children}
