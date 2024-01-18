0f278883f04e933ffdd3ff50c4f7d381a402bdd9
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-18 11:09:52 -0800

    feat: added logout button

--- clientside/chatapp/src/components/Navbar.js
+++ clientside/chatapp/src/components/Navbar.js
@@ -1,6 +1,10 @@
 import { Container, Nav, Navbar, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 const NavBar = () => {
+  const { user, logoutUser } = useContext(AuthContext);
+
   return (
     <Navbar bg="dark" className=" mb-4" style={{ height: "3.74rem" }}>
       <Container>
@@ -9,15 +13,37 @@ const NavBar = () => {
             ChattAp
           </Link>
         </h2>
-        <span className=" text-warning">Logged in</span>
+
+        {user && <span className=" text-warning">Logged in {user?.name}</span>}
         <Nav>
           <Stack direction="horizontal" gap={3}>
-            <Link to={"/login"} className=" link-light text-decoration-none ">
-              Login
-            </Link>
-            <Link to={"/register"} className="link-light text-decoration-none">
-              Register
-            </Link>
+            {user && (
+              <>
+                <Link
+                  onClick={() => logoutUser()}
+                  className=" link-light text-decoration-none "
+                >
+                  Logout
+                </Link>
+              </>
+            )}
+
+            {!user && (
+              <>
+                <Link
+                  to={"/login"}
+                  className=" link-light text-decoration-none "
+                >
+                  Login
+                </Link>
+                <Link
+                  to={"/register"}
+                  className="link-light text-decoration-none"
+                >
+                  Register
+                </Link>
+              </>
+            )}
           </Stack>
         </Nav>
       </Container>
