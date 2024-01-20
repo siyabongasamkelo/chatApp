60fad881d76c4ea294534f204d7ed2c464d270cc
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-20 10:23:00 -0800

    refactor: added styles & classes to the stack that displayes messages

--- clientside/chatapp/src/components/chat/ChatBox.js
+++ clientside/chatapp/src/components/chat/ChatBox.js
@@ -29,12 +29,21 @@ const ChatBox = () => {
       <div className="chat-header">
         <strong>{recipientUser?.name}</strong>
       </div>
-      <Stack gap={3} className="messages">
+      <Stack gap={3} className="messages text-white ">
         {messages &&
           messages.map((message, index) => (
-            <Stack key={index}>
+            <Stack
+              key={index}
+              className={`${
+                message?.senderId === user?.id
+                  ? "message self align-self-end flex-grow-0"
+                  : "message align-self-start flex-grow-0"
+              }`}
+            >
               <span>{message.text}</span>
-              <span>{moment(message.createAt).calender()}</span>
+              <span className="message-footer">
+                {moment(message?.createAt).calendar()}
+              </span>
             </Stack>
           ))}
       </Stack>
