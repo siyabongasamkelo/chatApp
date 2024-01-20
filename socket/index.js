153d858735956965cad8d170c04e68d7c227005d
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-20 13:03:51 -0800

    feat: created index.js file

---
+++ socket/index.js
@@ -0,0 +1,18 @@
+const { Server } = require("socket.io");
+
+const io = new Server({ cors: "http://localhost:5173" });
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+
+  socket.on("addNewUser", (userId) => {
+    !onlineUsers.some((user) => user.userId === userId) &&
+      onlineUsers.push({ userId, socketId: socket.id });
+  });
+
+  console.log("onlineUsers", onlineUsers);
+});
+
+io.listen(3001);
