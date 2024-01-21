62ff14f6b7ee10663accdcccdfc27ee3205f0b3e
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-21 08:55:40 -0800

    feat: created add & get message events on socket

--- socket/index.js
+++ socket/index.js
@@ -10,9 +10,27 @@ io.on("connection", (socket) => {
   socket.on("addNewUser", (userId) => {
     !onlineUsers.some((user) => user.userId === userId) &&
       onlineUsers.push({ userId, socketId: socket.id });
+
+    console.log("onlineUsers", onlineUsers);
+
+    io.emit("getOnlineUsers", onlineUsers);
   });
 
-  console.log("onlineUsers", onlineUsers);
+  //add messge
+  socket.on("sendMessage", (message) => {
+    const user = onlineUsers.find(
+      (user) => user.userId === message.recipientId
+    );
+
+    if (user) {
+      io.to(user.socketId).emit("getMessage", message);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
 });
 
 io.listen(3001);
