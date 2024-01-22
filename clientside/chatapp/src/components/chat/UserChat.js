6ac44c93eaa610fd7a521c4b04955b4d33cb6028
siyabonga <siyabongasamkelociam@gmail.com>
2024-01-22 07:05:37 -0800

    feat: added mark notification as read to the onClick

--- clientside/chatapp/src/components/chat/UserChat.js
+++ clientside/chatapp/src/components/chat/UserChat.js
@@ -3,14 +3,36 @@ import { useFetchRecientUser } from "../../hooks/useFetchReciept";
 import averter from "../../assets/avarter.svg";
 import { useContext } from "react";
 import { ChatContext } from "../../context/ChatContext";
+import { unReadNotificationsFunc } from "../../utils/unReadNotifications";
+import { useFetchLatestMessage } from "../../hooks/userFetchLatestMessage";
+import moment from "moment";
 
 const UserChat = (chat, user) => {
   const { recipientUser } = useFetchRecientUser(chat, user);
-  const { createChat, onlineUsers } = useContext(ChatContext);
+  const {
+    createChat,
+    onlineUsers,
+    notifications,
+    markThisUserNotificationAsRead,
+  } = useContext(ChatContext);
+  const { latestMessage } = useFetchLatestMessage(chat);
 
+  const unReadNotifications = unReadNotificationsFunc(notifications);
+  const thisUserNotifications = unReadNotifications?.filter(
+    (n) => n.senderId == recipientUser?.id
+  );
   const isOnline = onlineUsers?.some(
     (user) => user?.userId === recipientUser?._id
   );
+
+  const truncateText = (text) => {
+    let shortText = text.substring(0, 20);
+
+    if (text.length > 20) {
+      shortText = shortText + "...";
+    }
+    return shortText;
+  };
 
   return (
     <Stack
@@ -18,6 +40,11 @@ const UserChat = (chat, user) => {
       gap={3}
       className="user-card align-items-center p-2 justify-content-between "
       role="button"
+      onClick={() => {
+        if (thisUserNotifications?.length !== 0) {
+          markThisUserNotificationAsRead(thisUserNotifications, notifications);
+        }
+      }}
     >
       <div className=" d-flex ">
         <div className="me-2">
@@ -25,12 +52,26 @@ const UserChat = (chat, user) => {
         </div>
         <div className="text-content">
           <div className="name ">{recipientUser?.name}</div>
-          <div className="text">Text Message</div>
+          <div className="text">
+            {latestMessage?.text && (
+              <span>{truncateText(latestMessage?.text)}</span>
+            )}
+          </div>
         </div>
       </div>
       <div className=" d-flex flex-column align-items-end">
-        <div className="date">12/12/2023</div>
-        <div className="this-user-notifications">2</div>
+        <div className="date">
+          {moment(latestMessage?.createdAt).calendar()}
+        </div>
+        <div
+          className={
+            thisUserNotifications?.length > 0 ? "this-user-notifications" : ""
+          }
+        >
+          {thisUserNotifications?.length > 0
+            ? thisUserNotifications?.length
+            : ""}
+        </div>
         <span className={isOnline ? "user-online" : ""}></span>
       </div>
     </Stack>
